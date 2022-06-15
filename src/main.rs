2a6987c8c9b31af19303a04e20a6f6e27f1305dc
Collin O'Connor <collinoconnor2@gmail.com>
2022-06-15 05:08:08 -0400

    Find device based on name containing "wireless"

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,16 @@
 use windows_sys::{
     Win32::Devices::DeviceAndDriverInstallation::*,
+    Win32::Devices::Properties::DEVPKEY_Device_FriendlyName,
+    Win32::Devices::Properties::DEVPROP_TYPE_STRING,
 };
 
 fn main() {
     unsafe {
         let instance = get_wifi_device();
+
+        if instance == 0xFFFFFFFF {
+            panic!("No wireless device found.");
+        }
 
         let ret: CONFIGRET = CM_Disable_DevNode(instance, 0);
 
@@ -27,7 +33,7 @@ fn main() {
 unsafe fn get_wifi_device() -> u32 {
     let device_class_filter = b"{4d36e972-e325-11ce-bfc1-08002be10318}\0".as_ptr();
 
-    let mut len: u32 = 0;
+    let mut len = 0;
 
     let ret: CONFIGRET = CM_Get_Device_ID_List_SizeA(
         &mut len,
@@ -39,7 +45,7 @@ unsafe fn get_wifi_device() -> u32 {
         panic!("Failed to get device list size: error: {}", CM_MapCrToWin32Err(ret, 0));
     }
 
-    let mut buffer = vec![0 as u8; len as usize];
+    let mut buffer = vec![0u8; len as usize];
 
     let ret: CONFIGRET = CM_Get_Device_ID_ListA(
         device_class_filter,
@@ -52,25 +58,46 @@ unsafe fn get_wifi_device() -> u32 {
         panic!("Failed to get device list: error: {}", CM_MapCrToWin32Err(ret, 0));
     }
 
-    let list = buffer.into_iter().map(|c| c as char).collect::<String>();
+    let device_string = buffer.into_iter().map(|c| c as char).collect::<String>();
 
-    let wifi_device = list.split("\\0").nth(1).unwrap().to_string();
+    let list = device_string.split('\0').filter(|s| s.len() > 1);
 
-    let id_string = wifi_device.split('\0').nth(2).unwrap();
+    for device in list {
+        let mut friendly_name = [0u8; 256];
+        let mut name_sz = 256;
 
-    let id_addr = id_string.as_ptr() as *const i8;
+        let id_addr = device.as_ptr() as *const i8;
+        let mut instance_id = 0;
+        
+        let ret: CONFIGRET = CM_Locate_DevNodeA(
+            &mut instance_id,
+            id_addr,
+            CM_LOCATE_DEVNODE_NOVALIDATION
+        );
 
-    let mut instance_id: u32 = 0;
-    
-    let ret: CONFIGRET = CM_Locate_DevNodeA(
-        &mut instance_id,
-        id_addr,
-        CM_LOCATE_DEVNODE_NOVALIDATION
-    );
+        if ret != 0 {
+            panic!("Failed to get device instance id: error: {}", CM_MapCrToWin32Err(ret, 0));
+        }
+        
+        let ret: CONFIGRET = CM_Get_DevNode_PropertyW(
+            instance_id,
+            &DEVPKEY_Device_FriendlyName,
+            &mut DEVPROP_TYPE_STRING,
+            friendly_name.as_mut_ptr(),
+            &mut name_sz,
+            0
+        );
 
-    if ret != 0 {
-        panic!("Failed to get device instance id: error: {}", CM_MapCrToWin32Err(ret, 0));
+        if ret != 0 {
+            panic!("Failed to get device property: error: {}", CM_MapCrToWin32Err(ret, 0));
+        }
+
+        let name = friendly_name.into_iter().map(|c| c as char).filter(|c| *c != '\0').collect::<String>();
+
+        if name.to_ascii_lowercase().contains("wireless") {
+            return instance_id;
+        }
     }
 
-    instance_id
+    0xFFFFFFFF
 }
